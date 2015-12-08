e69202875093578a2dbb3c0860b0e13ba756a409
parkeunyoung <yyoung112@naver.com>
2015-12-08 17:20:17 +0900

    commit

---
+++ Study/src/com/study/interview/Interview_Q2.java
@@ -0,0 +1,14 @@
+package com.study.interview;
+/*
+ *  두 문자열을 입력 파라미터로 하여 같은 철자로 구성된 단어인지 판단하는 메소드를 작성하세요,
+ *  예)  example, plemaex 는 같은 철자로 구성되어 있습니다. 
+ *  이런 케이스에는 true를 그렇지 않으면 false를 반환하시면 됩니다. 
+ */
+public class Interview_Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Study/src/com/study/interview/Interview_Q3.java
@@ -0,0 +1,9 @@
+package com.study.interview;
+/*
+ * 아래와 같이 integer type의 배열이 있을 때 순서를 역순으로 정렬하는 reverse함수를 작성하세요. 
+   int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+   reverse(array);
+ */
+public class Interview_Q3 {
+	
+}
